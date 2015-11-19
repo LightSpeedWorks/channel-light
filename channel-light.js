31feaabcd18531e9203862764f16b8f4f55077fe
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-11-19 13:07:21 +0900

    channel-light.js

---
+++ channel-light.js
@@ -0,0 +1,35 @@
+void function () {
+	'use strict';
+
+	var slice = [].slice;
+
+	// Channel
+	function Channel() {
+		var recvs = slice.call(arguments), sends = [];
+		return function channel(first) {
+			var args = slice.call(arguments);
+			if (typeof first === 'function')
+				args.forEach(function (func) {
+					if (sends.length) func.apply(channel, sends.shift());
+					else recvs.push(func);
+				});
+			else if (first && typeof first.then === 'function')
+				first.then(channel, channel);
+			else {
+				if (first != null && !(first instanceof Error))
+					args = [null].concat(args);
+				if (args.length > 2) args = [args[0], args.slice(1)];
+				if (recvs.length) recvs.shift().apply(channel, args);
+				else sends.push(args);
+			}
+			return channel;
+		}
+	}
+
+	if (typeof module === 'object' && module && module.exports)
+		module.exports = Channel;
+
+	if (typeof window === 'object' && window)
+		window['channel-light'] = window.Channel = Channel;
+
+}();
